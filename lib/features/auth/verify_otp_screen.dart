e0fab776ff003005d5392bbcfda7664cb69a8450
raheeldevninja <raheelk.dev@gmail.com>
2024-03-28 23:42:44 +0500

    verify otp screen design, form validation, translations

--- lib/features/auth/verify_otp_screen.dart
+++ lib/features/auth/verify_otp_screen.dart
@@ -5,25 +5,20 @@ import 'package:mafatih/core/ui/header.dart';
 import 'package:mafatih/core/ui/simple_button.dart';
 import 'package:mafatih/core/ui/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:mafatih/core/util/utils.dart';
-import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
-import 'package:mafatih/features/auth/verify_otp_screen.dart';
 
 
-class ForgotPasswordScreen extends StatefulWidget {
-  const ForgotPasswordScreen({super.key});
+class VerifyOTPScreen extends StatefulWidget {
+  const VerifyOTPScreen({super.key});
 
   @override
-  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+  State<VerifyOTPScreen> createState() => _VerifyOTPScreenState();
 }
 
-class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+class _VerifyOTPScreenState extends State<VerifyOTPScreen> {
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  final _phoneNumberController = TextEditingController();
-
-  String selectedCountryCode = '+966';
+  final _otpController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,14 +28,12 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
-    print('language: ${l10n.localeName}');
-
     return Scaffold(
       backgroundColor: AppColors.secondaryBgColor,
       resizeToAvoidBottomInset: true,
       appBar: AppBar(
         backgroundColor: AppColors.secondaryColor,
-        title: Text(l10n.forgotPasswordHeading, style: const TextStyle(fontWeight: FontWeight.w500)),
+        title: Text(l10n.otpVerificationHeading, style: const TextStyle(fontWeight: FontWeight.w500)),
         centerTitle: true,
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -63,7 +56,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
       body: ListView(
         children: [
 
-          Header(subTitle: l10n.verifyNumberLabel),
+          Header(subTitle: l10n.verifyOTPSubTitle),
 
           Padding(
             padding: const EdgeInsets.all(16.0),
@@ -72,22 +65,20 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
               child: Column(
                 children: [
 
-                  Widgets.labels(l10n.phoneNoLabel),
+                  Widgets.labels(l10n.otpCodeLabel),
                   const SizedBox(
                     height: 10,
                   ),
 
                   //rounded corner with border text form field
                   AppTextField(
-                    controller: _phoneNumberController,
-                    keyboardType: TextInputType.phone,
-                    hintText: '',
-                    isPhone: true,
-                    countryPickerCallback: _showCountryCodeBottomSheet,
+                    controller: _otpController,
+                    keyboardType: TextInputType.number,
+                    hintText: l10n.otpCodeHint,
                     validator: (value) {
 
                       if (value!.isEmpty) {
-                        return l10n.emptyPhoneValidation;
+                        return l10n.emptyOTPValidation;
                       }
 
                       return null;
@@ -117,14 +108,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
 
               if (_formKey.currentState!.validate()) {
 
-                Utils.showCustomSnackBar(context, 'OTP sent', ContentType.success);
-
-                ///navigate to verify otp screen
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const VerifyOTPScreen(),
-                  ),
-                );
+                ///Navigate to new password screen
 
 
               }
@@ -136,53 +120,11 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
     );
   }
 
-
-  void _showCountryCodeBottomSheet() {
-
-    final l10n = AppLocalizations.of(context)!;
-
-    showModalBottomSheet(
-      context: context,
-      builder: (context) {
-        return Container(
-          height: 300,
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Text(l10n.selectCountryCodeHeading),
-              const SizedBox(height: 10),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: const Text('+966'),
-                      onTap: () {
-
-                        setState(() {
-                          selectedCountryCode = '+966';
-                        });
-
-                        Navigator.pop(context);
-
-                      },
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-
   @override
   void dispose() {
     super.dispose();
 
-    _phoneNumberController.dispose();
+    _otpController.dispose();
   }
 
 }
